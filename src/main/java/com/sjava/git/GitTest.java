e25051db07ddac14f11e4729402476a4fcda0236
Lisi <Lisi>
2022-03-26 17:48:06 +0800

    second commit

--- src/main/java/com/sjava/git/GitTest.java
+++ src/main/java/com/sjava/git/GitTest.java
@@ -3,5 +3,6 @@ package com.sjava.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
